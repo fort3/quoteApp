cb3478bdb2105299ce30b4776271a0f17bb71d07
Fortune Sam Okon <macintosh@Macintoshs-MacBook-Pro.local>
2020-05-13 23:20:19 +0200

    Updates

---
+++ precache-manifest.7b0e2e1fb68979cbd4a88ca966e3c595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3f3edebf56edc886334004a11e8150",
+    "url": "/quoteApp/index.html"
+  },
+  {
+    "revision": "a9eb46e2bb1af019debf",
+    "url": "/quoteApp/static/css/main.8829ddaf.chunk.css"
+  },
+  {
+    "revision": "5b439591525bf64eab14",
+    "url": "/quoteApp/static/js/2.dca88d1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quoteApp/static/js/2.dca88d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9eb46e2bb1af019debf",
+    "url": "/quoteApp/static/js/main.476abac2.chunk.js"
+  },
+  {
+    "revision": "9fb15b37c91386f71d42",
+    "url": "/quoteApp/static/js/runtime-main.615521a0.js"
+  }
+]);
